c59a3879eb2e726fcb95bcc5f8f847d9a077d1f0
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-27 19:33:48 -0300

    Update apigateway.tf

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -37,18 +37,6 @@ resource "aws_api_gateway_model" "empty_model" {
   })
 }
 
-# 200 Response
-resource "aws_api_gateway_method_response" "proxy_method_response_200" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = aws_api_gateway_model.empty_model.name
-  }
-}
-
 resource "aws_api_gateway_integration_response" "proxy_integration_response_200" {
   rest_api_id     = aws_api_gateway_rest_api.my_api.id
   resource_id     = aws_api_gateway_resource.proxy.id
@@ -61,18 +49,6 @@ resource "aws_api_gateway_integration_response" "proxy_integration_response_200"
   }
 }
 
-# 201 Response
-resource "aws_api_gateway_method_response" "proxy_method_response_201" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  status_code = "201"
-
-  response_models = {
-    "application/json" = aws_api_gateway_model.empty_model.name
-  }
-}
-
 resource "aws_api_gateway_integration_response" "proxy_integration_response_201" {
   rest_api_id     = aws_api_gateway_rest_api.my_api.id
   resource_id     = aws_api_gateway_resource.proxy.id
@@ -82,18 +58,6 @@ resource "aws_api_gateway_integration_response" "proxy_integration_response_201"
 
   response_templates = {
     "application/json" = "" # Não transforma o corpo
-  }
-}
-
-# 400 Response
-resource "aws_api_gateway_method_response" "proxy_method_response_400" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  status_code = "400"
-
-  response_models = {
-    "application/json" = aws_api_gateway_model.empty_model.name
   }
 }
 
@@ -112,11 +76,8 @@ resource "aws_api_gateway_integration_response" "proxy_integration_response_400"
 resource "aws_api_gateway_deployment" "my_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.proxy_integration,
-    aws_api_gateway_method_response.proxy_method_response_200,
     aws_api_gateway_integration_response.proxy_integration_response_200,
-    aws_api_gateway_method_response.proxy_method_response_201,
     aws_api_gateway_integration_response.proxy_integration_response_201,
-    aws_api_gateway_method_response.proxy_method_response_400,
     aws_api_gateway_integration_response.proxy_integration_response_400
   ]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
