309352bf5f891b7a2d115b300bef7cdf95e9a73a
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-27 18:59:53 -0300

    Update apigateway.tf

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -24,10 +24,97 @@ resource "aws_api_gateway_integration" "proxy_integration" {
   type                    = "HTTP"
 
   uri                     = "http://a55fb585ed9f94fc399f66f3f60f5e96-913860324.us-east-1.elb.amazonaws.com:3000/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "{proxy}"
+  }
+}
+
+# 200 Response
+resource "aws_api_gateway_method_response" "proxy_method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "200"
+
+  response_model_id = null
+}
+
+resource "aws_api_gateway_integration_response" "proxy_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Content-Type" = "integration.response.header.Content-Type"
+  }
+
+  response_templates = {
+    "application/json" = "" # Não transforma o corpo
+  }
+}
+
+# 201 Response
+resource "aws_api_gateway_method_response" "proxy_method_response_201" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "201"
+
+  response_model_id = null
+}
+
+resource "aws_api_gateway_integration_response" "proxy_integration_response_201" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "201"
+
+  response_parameters = {
+    "method.response.header.Content-Type" = "integration.response.header.Content-Type"
+  }
+
+  response_templates = {
+    "application/json" = "" # Não transforma o corpo
+  }
+}
+
+# 400 Response
+resource "aws_api_gateway_method_response" "proxy_method_response_400" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "400"
+
+  response_model_id = null
+}
+
+resource "aws_api_gateway_integration_response" "proxy_integration_response_400" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "400"
+
+  response_parameters = {
+    "method.response.header.Content-Type" = "integration.response.header.Content-Type"
+  }
+
+  response_templates = {
+    "application/json" = "" # Não transforma o corpo
+  }
 }
 
 resource "aws_api_gateway_deployment" "my_api_deployment" {
-  depends_on = [aws_api_gateway_integration.proxy_integration]
+  depends_on = [
+    aws_api_gateway_integration.proxy_integration,
+    aws_api_gateway_method_response.proxy_method_response_200,
+    aws_api_gateway_integration_response.proxy_integration_response_200,
+    aws_api_gateway_method_response.proxy_method_response_201,
+    aws_api_gateway_integration_response.proxy_integration_response_201,
+    aws_api_gateway_method_response.proxy_method_response_400,
+    aws_api_gateway_integration_response.proxy_integration_response_400
+  ]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name  = "v1"
 }
