a951d3a4db7739631571c5d2e939f4932fe07cc4
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-28 17:56:05 -0300

    vpc link para acesso ao load balancer privado

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "k8s" {
   zone_az1     = "us-east-1a"
   zone_az2     = "us-east-1b"
   iam_role_arn = "arn:aws:iam::125427248349:role/LabRole"
-  url_load_balance = "arn:aws:elasticloadbalancing:us-east-1:125427248349:listener/net/aadfa33dba232459c953a70b3381a909/617e9a439eaa8419/d4914d423e4ea231"
+  url_load_balance = "arn:aws:elasticloadbalancing:us-east-1:125427248349:loadbalancer/net/aadfa33dba232459c953a70b3381a909/617e9a439eaa8419"
 }
