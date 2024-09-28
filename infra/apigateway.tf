3bc936344352f7208c7ad9ea21f70c3804004fbd
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-28 15:22:12 -0300

    vpc link para acesso ao load balancer privado

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -3,6 +3,7 @@ resource "aws_vpc_endpoint" "apigateway" {
   vpc_id            = module.vpc.vpc_id
   service_name      = "com.amazonaws.us-east-1.execute-api"
   vpc_endpoint_type = "Interface"
+  subnet_ids = module.vpc.private_subnets
   security_group_ids = [aws_security_group.eks_security_group.id]
 
   depends_on = [
