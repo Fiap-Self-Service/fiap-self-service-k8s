a7c29563ab470e99040096f1ae21d9efc666af45
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-27 21:10:39 -0300

    Update apigateway.tf

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -1,35 +1,38 @@
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "FiapSelfServiceAPI"
-  description = "API Gateway FiapSelfService"
+resource "aws_api_gateway_rest_api" "fiap_api" {
+  name        = "Fiap Self Service API"
+  description = "Fiap API"
 }
 
 resource "aws_api_gateway_resource" "default" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.fiap_api.id
+  parent_id   = aws_api_gateway_rest_api.fiap_api.root_resource_id
   path_part   = "$default"
 }
 
 resource "aws_api_gateway_method" "default_method" {
-  rest_api_id   = aws_api_gateway_rest_api.example.id
+  rest_api_id   = aws_api_gateway_rest_api.fiap_api.id
   resource_id   = aws_api_gateway_resource.default.id
   http_method   = "ANY"  # Aceita todos os métodos HTTP
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "redirect_integration" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
+resource "aws_api_gateway_integration" "http_proxy_integration" {
+  rest_api_id = aws_api_gateway_rest_api.fiap_api.id
   resource_id = aws_api_gateway_resource.default.id
   http_method = aws_api_gateway_method.default_method.http_method
 
-  type                     = "HTTP"
+  type                     = "HTTP_PROXY"
   uri                      = "http://a55fb585ed9f94fc399f66f3f60f5e96-913860324.us-east-1.elb.amazonaws.com:3000"
+  connection_type          = "INTERNET"
+  request_parameters       = {}
+  request_templates        = {}
 }
 
-resource "aws_api_gateway_deployment" "example_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
+resource "aws_api_gateway_deployment" "fiap_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.fiap_api.id
   stage_name  = "v1"
 }
 
 output "invoke_url" {
-  value = "${aws_api_gateway_deployment.example_deployment.invoke_url}"
+  value = "${aws_api_gateway_deployment.fiap_api_deployment.invoke_url}"
 }
