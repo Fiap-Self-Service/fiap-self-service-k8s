107293074bc5a0c6cf3520e54e3ea30c92a7321b
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-27 21:02:24 -0300

    Update apigateway.tf

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -1,84 +1,36 @@
-resource "aws_api_gateway_rest_api" "my_api" {
-  name        = "MyAPI"
-  description = "My API Gateway"
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "FiapSelfServiceAPI"
+  description = "API Gateway FiapSelfService"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part   = "{proxy+}"
+resource "aws_api_gateway_resource" "default" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "$default"
 }
 
-resource "aws_api_gateway_method" "proxy_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
+resource "aws_api_gateway_method" "default_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.default.id
+  http_method   = "ANY"  # Aceita todos os métodos HTTP
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "proxy_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.id
-  resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.proxy_method.http_method
-  integration_http_method = "ANY"
-  type                    = "HTTP"
+resource "aws_api_gateway_integration" "redirect_integration" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.default.id
+  http_method = aws_api_gateway_method.default_method.http_method
 
-  uri = "http://a55fb585ed9f94fc399f66f3f60f5e96-913860324.us-east-1.elb.amazonaws.com:3000/{proxy}"
+  type                     = "HTTP"
+  uri                      = "http://a55fb585ed9f94fc399f66f3f60f5e96-913860324.us-east-1.elb.amazonaws.com:3000"
+  integration_http_method  = "ANY"
 }
 
-# Modelo de resposta vazio
-resource "aws_api_gateway_model" "empty_model" {
-  rest_api_id  = aws_api_gateway_rest_api.my_api.id
-  name         = "EmptyModel"
-  content_type = "application/json"
-  schema = jsonencode({
-    type       = "object"
-    properties = {}
-  })
-}
-
-# 200 Response
-resource "aws_api_gateway_method_response" "proxy_method_response_200" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = aws_api_gateway_model.empty_model.name
-  }
-}
-
-# 201 Response
-resource "aws_api_gateway_method_response" "proxy_method_response_201" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  status_code = "201"
-
-  response_models = {
-    "application/json" = aws_api_gateway_model.empty_model.name
-  }
-}
-
-# 400 Response
-resource "aws_api_gateway_method_response" "proxy_method_response_400" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-  status_code = "400"
-
-  response_models = {
-    "application/json" = aws_api_gateway_model.empty_model.name
-  }
-}
-
-resource "aws_api_gateway_deployment" "my_api_deployment" {
-  depends_on = [
-    aws_api_gateway_method_response.proxy_method_response_200,
-    aws_api_gateway_method_response.proxy_method_response_201,
-    aws_api_gateway_method_response.proxy_method_response_400
-  ]
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
+resource "aws_api_gateway_deployment" "example_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
   stage_name  = "v1"
+}
+
+output "invoke_url" {
+  value = "${aws_api_gateway_deployment.example_deployment.invoke_url}"
 }
