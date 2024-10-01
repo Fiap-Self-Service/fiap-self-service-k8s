817526a0fd9e802e668e7b79896ccbfd85b70f7c
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-09-30 21:43:20 -0300

    Update cognito.tf

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -45,3 +45,7 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
 output "user_pool_id" {
   value = aws_cognito_user_pool.user_pool.id
 }
+
+output "client_id" {
+  value = aws_cognito_user_pool_client.user_pool_client.id
+}
