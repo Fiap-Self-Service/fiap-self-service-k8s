6eb07cd04c1d9d2cf8faa9bf7d29313d3ee47da4
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-11-20 15:28:41 -0300

    Ajuste IAM labRole

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ module "k8s" {
   region       = "us-east-1"
   zone_az1     = "us-east-1a"
   zone_az2     = "us-east-1b"
-  iam_role_arn = "arn:aws:iam::125427248349:role/LabRole"
+  iam_role_arn = "arn:aws:iam::629468475927:role/LabRole"
   url_load_balance = "http://aadfa33dba232459c953a70b3381a909-617e9a439eaa8419.elb.us-east-1.amazonaws.com:3000"
 }
