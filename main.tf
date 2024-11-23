8c0447342d2b9f868bc26a020ff536b2ed7e7b74
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-11-23 17:06:35 -0300

    api gateway - rota clientes

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "k8s" {
   zone_az1     = "us-east-1a"
   zone_az2     = "us-east-1b"
   iam_role_arn = "arn:aws:iam::629468475927:role/LabRole"
-  url_load_balance_clientes = "http://ab2d76323dda749e2a825b9d7eb44e5e-95224636.us-east-1.elb.amazonaws.com:3000/clientes/"
+  url_load_balance_clientes = "http://ab2d76323dda749e2a825b9d7eb44e5e-95224636.us-east-1.elb.amazonaws.com:3000/clientes"
 }
