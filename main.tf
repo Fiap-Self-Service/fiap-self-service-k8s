c7abb9021d2047c8e3f833639e01bd25697f6de1
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-11-24 18:11:44 -0300

    config api gateway

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "k8s" {
   zone_az1     = "us-east-1a"
   zone_az2     = "us-east-1b"
   iam_role_arn = "arn:aws:iam::629468475927:role/LabRole"
-  url_load_balance_clientes = "http://a2d78a18e02f34026bc8eae7884eebf9-1640504450.us-east-1.elb.amazonaws.com:3000/clientes/\${proxy}"
+  url_load_balance_clientes = "http://a2d78a18e02f34026bc8eae7884eebf9-1640504450.us-east-1.elb.amazonaws.com:3000/clientes/$${proxy}"
 }
