4df02e7f6b8379af307ad9f2cc385a6068e8f0d6
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-11-30 08:46:08 -0300

    Deploy fase4

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ module "k8s" {
   region       = "us-east-1"
   zone_az1     = "us-east-1a"
   zone_az2     = "us-east-1b"
-  iam_role_arn = "arn:aws:iam::629468475927:role/LabRole"
+  iam_role_arn = "arn:aws:iam::657290934315:role/LabRole"
   url_load_balance_clientes = "http://a2d78a18e02f34026bc8eae7884eebf9-1640504450.us-east-1.elb.amazonaws.com:3000/clientes/$${proxy}"
 }
